6265ec3c683a43bd2c39c22e27c491c2fc2a744e
RogerDodger <roger.dodger.thor@gmail.com>
2017-10-20 03:42:07 +0800

    whitespace

--- src/App.js
+++ src/App.js
@@ -8,74 +8,74 @@ import 'typeface-roboto';
 import './App.css';
 
 class App extends Component {
-  state = { user: null };
+   state = { user: null };
 
-  async componentWillMount() {
-    let user = localStorage.getItem('user');
-    if (user) {
-      this.setState({ user: JSON.parse(user) });
-    }
+   async componentWillMount() {
+      let user = localStorage.getItem('user');
+      if (user) {
+         this.setState({ user: JSON.parse(user) });
+      }
 
-    let res = await fetchapi('/me');
-    let json = await res.json();
-    if ('user' in json) {
-      this.setState({ user: json.user });
-      localStorage.setItem('user', JSON.stringify(this.state.user));
-    }
-  }
+      let res = await fetchapi('/me');
+      let json = await res.json();
+      if ('user' in json) {
+         this.setState({ user: json.user });
+         localStorage.setItem('user', JSON.stringify(this.state.user));
+      }
+   }
 
-  handleLogin = async (username) => {
-    let form = new FormData();
-    form.set('username', username);
+   handleLogin = async (username) => {
+      let form = new FormData();
+      form.set('username', username);
 
-    let res = await fetchapi('/login', {
-      method: 'post',
-      body: form,
-    });
-    let json = await res.json();
+      let res = await fetchapi('/login', {
+         method: 'post',
+         body: form,
+      });
+      let json = await res.json();
 
-    if ('user' in json) {
-      this.setState({ user: json.user });
-      localStorage.setItem('user', JSON.stringify(this.state.user));
-    }
-  }
+      if ('user' in json) {
+         this.setState({ user: json.user });
+         localStorage.setItem('user', JSON.stringify(this.state.user));
+      }
+   }
 
-  handleLogout = async () => {
-    let res = await fetchapi('/logout', { method: 'post' });
-    if (res.ok) {
-      this.setState({ user: null });
-      localStorage.removeItem('user');
-    }
-  }
+   handleLogout = async () => {
+      let res = await fetchapi('/logout', { method: 'post' });
+      if (res.ok) {
+         this.setState({ user: null });
+         localStorage.removeItem('user');
+      }
+   }
 
-  render() {
-    const user = this.state.user;
+   render() {
+      const user = this.state.user;
 
-    return (
-      <Router>
-        <div className="App">
-          <ToastContainer />
-          <div className="App-header">
-            <div className="App-header--user">
-              { user === null
-                ? <LoginForm handleLogin={this.handleLogin} />
-                : (
-                  <div>
-                    <span>{ user.name }</span>
-                    <Logout handleLogout={this.handleLogout} />
+      return (
+         <Router>
+            <div className="App">
+               <ToastContainer />
+               <div className="App-header">
+                  <div className="App-header--user">
+                     { user === null
+                        ? <LoginForm handleLogin={this.handleLogin} />
+                        : (
+                           <div>
+                              <span>{ user.name }</span>
+                              <Logout handleLogout={this.handleLogout} />
+                           </div>
+                           )}
                   </div>
-                  )}
+               </div>
+               <div className="App-main">
+               { user &&
+                  <Route path="/" component={Rank} />}
+               </div>
+               <div className="App-footer"/>
             </div>
-          </div>
-          <div className="App-main">
-          { user &&
-            <Route path="/" component={Rank} />}
-          </div>
-          <div className="App-footer"/>
-        </div>
-      </Router>
-    );
-  }
+         </Router>
+      );
+   }
 }
 
 export default App;
--- src/Rank.js
+++ src/Rank.js
@@ -8,181 +8,181 @@ import 'react-select/dist/react-select.css';
 import './Rank.css';
 
 function heroImgUrl (name) {
-  return `https://cdn.steamstatic.com/apps/dota2/images/heroes/${name}_full.png`;
+   return `https://cdn.steamstatic.com/apps/dota2/images/heroes/${name}_full.png`;
 };
 
 class HeroAdd extends Component {
-  state = { value: '' };
+   state = { value: '' };
 
-  handleChange = (e) => {
-    this.setState({ value: e ? e.value : '' });
-  };
+   handleChange = (e) => {
+      this.setState({ value: e ? e.value : '' });
+   };
 
-  handleClick = (e) => {
-    if (e.target.className === 'Hero-add-plus' || e.target.className === 'Hero-add-container') {
-      if (this.state.value !== '') {
-        this.props.appendHero(this.state.value);
-        this.setState({ value: '' });
+   handleClick = (e) => {
+      if (e.target.className === 'Hero-add-plus' || e.target.className === 'Hero-add-container') {
+         if (this.state.value !== '') {
+            this.props.appendHero(this.state.value);
+            this.setState({ value: '' });
+         }
       }
-    }
-  };
+   };
 
-  render() {
-    let selected = {};
-    this.props.heroes.forEach((e) => selected[e.id] = true);
-    let options = heroList
-        .filter((e) => !selected[e.id])
-        .sort((a, b) => a.localized_name.localeCompare(b.localized_name))
-        .map((e) => { return { value: e.name, label: e.localized_name }; });
+   render() {
+      let selected = {};
+      this.props.heroes.forEach((e) => selected[e.id] = true);
+      let options = heroList
+            .filter((e) => !selected[e.id])
+            .sort((a, b) => a.localized_name.localeCompare(b.localized_name))
+            .map((e) => { return { value: e.name, label: e.localized_name }; });
 
-    let style = {};
-    if (this.state.value !== '') {
-      style['backgroundImage'] = 'url(' + heroImgUrl(this.state.value) + ')';
-    }
+      let style = {};
+      if (this.state.value !== '') {
+         style['backgroundImage'] = 'url(' + heroImgUrl(this.state.value) + ')';
+      }
 
-    return (
-      <div className="Hero-add">
-        <div className="Hero-add-container" style={style} onClick={this.handleClick}>
-          <Select
-            className="Hero-add-field"
-            value={this.state.value}
-            onChange={this.handleChange}
-            options={options}
-          />
-          <div className="Hero-add-plus">+</div>
-        </div>
-      </div>
-    )
-  }
+      return (
+         <div className="Hero-add">
+            <div className="Hero-add-container" style={style} onClick={this.handleClick}>
+               <Select
+                  className="Hero-add-field"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  options={options}
+               />
+               <div className="Hero-add-plus">+</div>
+            </div>
+         </div>
+      )
+   }
 }
 
 
 class Hero extends Component {
-  startDelete = () => {
-    this.props.deleteHero(this.props.hero);
-  }
+   startDelete = () => {
+      this.props.deleteHero(this.props.hero);
+   }
 
-  updateNote = (e) => {
-    this.props.updateNote(this.props.hero, e.target.value);
-  }
+   updateNote = (e) => {
+      this.props.updateNote(this.props.hero, e.target.value);
+   }
 
-  render() {
-    const h = this.props.hero;
-    const s = { backgroundImage : 'url(' + heroImgUrl(h.name) + ')' };
+   render() {
+      const h = this.props.hero;
+      const s = { backgroundImage : 'url(' + heroImgUrl(h.name) + ')' };
 
-    return (
-      <div className="Hero" style={s} title={h.localized_name}>
-        <div className="Hero-controls">
-          <textarea
-            className="Hero-note"
-            value={h.note}
-            onChange={this.updateNote} />
-          <div className="Hero-delete" onClick={this.startDelete}>
-            &times;
-          </div>
-        </div>
-      </div>
-    );
-  }
+      return (
+         <div className="Hero" style={s} title={h.localized_name}>
+            <div className="Hero-controls">
+               <textarea
+                  className="Hero-note"
+                  value={h.note}
+                  onChange={this.updateNote} />
+               <div className="Hero-delete" onClick={this.startDelete}>
+                  &times;
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
 
 const SortableHero = SortableElement(Hero);
 
 const HeroList = SortableContainer((props) => {
-  return (
-    <div className="Hero-list">
-      { props.heroes.map((e, i) => (
-        <SortableHero
-          hero={e} index={i} key={e.id}
-          deleteHero={props.deleteHero}
-          updateNote={props.updateNote} />
-      )) }
-    </div>
-  )
+   return (
+      <div className="Hero-list">
+         { props.heroes.map((e, i) => (
+            <SortableHero
+               hero={e} index={i} key={e.id}
+               deleteHero={props.deleteHero}
+               updateNote={props.updateNote} />
+         )) }
+      </div>
+   )
 });
 
 class Role extends Component {
-  state = {
-    heroes: heroList.slice(0, 10).map((e) => Object.assign({}, e))
-  };
+   state = {
+      heroes: heroList.slice(0, 10).map((e) => Object.assign({}, e))
+   };
 
-  onSortEnd = ({oldIndex, newIndex}) => {
-    this.setState({
-      heroes: arrayMove(this.state.heroes, oldIndex, newIndex),
-    });
-  };
+   onSortEnd = ({oldIndex, newIndex}) => {
+      this.setState({
+         heroes: arrayMove(this.state.heroes, oldIndex, newIndex),
+      });
+   };
 
-  appendHero = (name) => {
-    this.setState({
-      heroes: this.state.heroes.concat(heroList.find((e) => e.name === name))
-    });
-  };
+   appendHero = (name) => {
+      this.setState({
+         heroes: this.state.heroes.concat(heroList.find((e) => e.name === name))
+      });
+   };
 
-  deleteHero = (hero) => {
-    const oldIndex = this.state.heroes.indexOf(hero);
+   deleteHero = (hero) => {
+      const oldIndex = this.state.heroes.indexOf(hero);
 
-    this.setState({
-      heroes: this.state.heroes.filter((e) => e !== hero)
-    });
+      this.setState({
+         heroes: this.state.heroes.filter((e) => e !== hero)
+      });
 
-    toast.send(
-      `${hero.localized_name} removed`,
-      () => { this.undoDeleteHero(hero, oldIndex); },
-    )
-  };
+      toast.send(
+         `${hero.localized_name} removed`,
+         () => { this.undoDeleteHero(hero, oldIndex); },
+      )
+   };
 
-  undoDeleteHero = (hero, index) => {
-    console.log(hero, index);
-    let heroes = this.state.heroes.slice();
-    heroes.splice(index, 0, hero);
-    this.setState({ heroes: heroes });
-  };
+   undoDeleteHero = (hero, index) => {
+      console.log(hero, index);
+      let heroes = this.state.heroes.slice();
+      heroes.splice(index, 0, hero);
+      this.setState({ heroes: heroes });
+   };
 
-  updateNote = (hero, note) => {
-    this.setState({
-      heroes: this.state.heroes.map((e) => {
-        if (e === hero) {
-          return Object.assign(e, { note: note });
-        }
-        return e;
+   updateNote = (hero, note) => {
+      this.setState({
+         heroes: this.state.heroes.map((e) => {
+            if (e === hero) {
+               return Object.assign(e, { note: note });
+            }
+            return e;
+         })
       })
-    })
-  }
+   }
 
-  render() {
-    return (
-      <div className="Role">
-        <div className="Role-name">
-          {this.props.role.name}
-        </div>
-        <HeroList
-          heroes={this.state.heroes}
-          onSortEnd={this.onSortEnd}
-          deleteHero={this.deleteHero}
-          updateNote={this.updateNote}
-          distance={2}
-          lockAxis='y'
-          lockToContainerEdges={true}
-          lockOffset={0}
-        />
-        <HeroAdd heroes={this.state.heroes} appendHero={this.appendHero} />
-      </div>
-    )
-  }
+   render() {
+      return (
+         <div className="Role">
+            <div className="Role-name">
+               {this.props.role.name}
+            </div>
+            <HeroList
+               heroes={this.state.heroes}
+               onSortEnd={this.onSortEnd}
+               deleteHero={this.deleteHero}
+               updateNote={this.updateNote}
+               distance={2}
+               lockAxis='y'
+               lockToContainerEdges={true}
+               lockOffset={0}
+            />
+            <HeroAdd heroes={this.state.heroes} appendHero={this.appendHero} />
+         </div>
+      )
+   }
 }
 
 class Rank extends Component {
-  render() {
-    return (
-      <div className="Rank">
-        <div className="Roles">
-          { roleList.map((e, i) =>
-            <Role role={e} key={e.pos} />
-          )}
-        </div>
-      </div>
-    )
-  }
+   render() {
+      return (
+         <div className="Rank">
+            <div className="Roles">
+               { roleList.map((e, i) =>
+                  <Role role={e} key={e.pos} />
+               )}
+            </div>
+         </div>
+      )
+   }
 }
 
 export { Rank };
